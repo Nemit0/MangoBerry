eaf21c7f22989dd44a4a4cb79722ae152884bc0b
jiwon <jjwonn2@naver.com>
2025-07-02 09:41:15 +0900

    update

--- backend/es_sqlite.py
+++ backend/es_sqlite.py
@@ -25,7 +25,7 @@ def extract_categories(type_str):
 
 
 # Connect to SQLite
-conn = sqlite3.connect("clean_copy.sqlite")
+conn = sqlite3.connect("routers/clean_copy.sqlite")
 cursor = conn.cursor()
 
 # Fetch all columns
