04437c90ce6c8fd7b5a71b1bda11bda6ae5183c3
min <minhuijeong380@gmail.com>
2025-07-10 10:35:00 +0900

    팀장님 수정 프론트

--- frontend/src/pages/FollowingPage.js
+++ frontend/src/pages/FollowingPage.js
@@ -1,7 +1,39 @@
+// src/pages/FollowingPage.js
+import React from "react";
+import "./FollowingPage.css";
+
 const FollowingPage = () => {
-    return (
-        <div>FollowingPage</div>
-    );
+  const dummyFollowing = [
+    { name: "NICK", percent: 100 },
+    { name: "BOB", percent: 70 },
+    { name: "STEVE", percent: 60 },
+    { name: "EMILY", percent: 80 },
+    { name: "JULIA", percent: 60 },
+    { name: "JOHN", percent: 100 },
+  ];
+
+  return (
+    <div className="following-page">
+      <h2 className="following-title">팔로잉</h2>
+      <div className="keyword-result-container">
+        {dummyFollowing.map((user, index) => (
+          <div key={index} className="keyword-box">
+            <div className="profile-icon">👤</div>
+            <div className="name-and-bar">
+              <div className="user-name">{user.name}</div>
+              <div className="progress-bar-background">
+                <div
+                  className="progress-bar-fill"
+                  style={{ width: `${user.percent}%` }}
+                ></div>
+                <span className="percent-text">{user.percent}%</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FollowingPage;
