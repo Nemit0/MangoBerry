3bc0b40960329a665ff21fddb8ec693011cd5a0b
jiwon <jjwonn2@naver.com>
2025-07-11 09:11:10 +0900

    AsyncIOMotorCollection KeywordItem 추가 /keyword/{user_id} api 연결

--- backend/connection/mongodb.py
+++ backend/connection/mongodb.py
@@ -8,7 +8,7 @@ uri = os.getenv("MONGO_URI")
 client = MongoClient(uri)
 
 db = client["customer_info"]
-
+words_collection = db["words"]
 photo_collection = db["review_photos"]
 follow_collection = db["follow"]
 restaurant_keywords_collection = db["keywords"]
