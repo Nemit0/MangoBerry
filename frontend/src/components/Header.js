b0618377c0e38038ea3a5fee2cf98fb2daa9ca99
ktw <tawan4719@gmail.com>
2025-07-21 19:32:35 +0900

    0721 김태완 frontend 변경점 수업 이후 합친 후

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -51,8 +51,8 @@ function Header({ searchTerm, onSearchChange }) { // 검색어 관련 props 받�
                 <nav className="menu-nav">
                     {isLoggedIn ? (
                         <>
-                            <p onClick={goToMyPage} className="nav-link" style={{ cursor: 'pointer' }}>내 프로필</p>
-                            <p onClick={goToNewPage} className="nav-link" style={{ cursor: 'pointer' }}>새글쓰기</p>
+                            <p onClick={goToMyPage} className="nav-link" style={{ cursor: 'pointer' }}>프로필</p>
+                            <p onClick={goToNewPage} className="nav-link" style={{ cursor: 'pointer' }}>글쓰기</p>
                             <p onClick={goToMapPage} className="nav-link" style={{ cursor: 'pointer' }}>지도</p>
                         </>
                     ) : (
